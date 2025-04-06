e4ded4ff8f601a5766ee46c570246bfa46d31f33
tbcarus <70680359+tbcarus@users.noreply.github.com>
2025-04-06 19:03:52 +0300

    Init commit

---
+++ src/main/java/ru/tbcarus/photocloudserver/PhotoCloudServerApplication.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.photocloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoCloudServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbcarus/photocloudserver/PhotoCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.photocloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoCloudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
